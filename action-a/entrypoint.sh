3d82e4bfca3bf85953a057d8159e7dbeaecafd00
Ivaldino Soares <vadysoares@gmail.com>
2021-11-04 21:50:22 +0000

    mm

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 CLANG_FORMAT_VERSION="13"
 
@@ -14,17 +14,21 @@ function install_clang(){
 
 
 function clang_file() {
-		
+	
+    log_ "Run running clang on ${1}"
+    
     local files=$(find ${1} -regex '.*\.\(cpp\|hpp\|h\|cc\|cxx\)'  | tr '\n' ' ')
-
-    log_ "Run running clang"
+    
+    log_ "files found: ${files}"
+    
     
     /usr/bin/clang-format-${CLANG_FORMAT_VERSION} -n --Werror --style=file --fallback-style=Google ${files} || local format_status=$?
 		
 	if [[ "${format_status}" -ne 0 ]]; then
 		log_ "Run clang-format before commit"
-		return ${format_status}
 	fi
+	
+	return ${format_status} 
 	
 }
 
@@ -33,5 +37,7 @@ cd ${GITHUB_WORKSPACE}
 install_clang
 
 clang_file ${GITHUB_WORKSPACE} || exit_code=$?
+
+log_ "Job Completed"
 
 exit $exit_code 
