2b52f87a95b173beeb518c9477057cf1cc22e3b4
Ivaldino Soares <vadysoares@gmail.com>
2021-11-04 21:04:34 +0000

    Test script

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,11 +1,37 @@
-#!/bin/sh -l
+#!/bin/bash -e
 
-sh -c "echo Hello world my name is $INPUT_MY_NAME"
+CLANG_FORMAT_VERSION="13"
 
-ls -ltrh /github/workspace
-
-cd /github/workspace
-
-ls -ltrh .
+function log_() {
+    echo "[$(basename "$0")] $*"
+}
 
 
+function install_clang(){
+    log_ "Installing clang-format-$CLANG_FORMAT_VERSION"
+    apt-get update && apt-get install -y --no-install-recommends clang-format-"$CLANG_FORMAT_VERSION"
+}
+
+
+function clang_file() {
+		
+    local files=$(find ./src -regex '.*\.\(cpp\|hpp\|h\|cc\|cxx\)'  | tr '\n' ' ')
+
+    log_ "Run running clang"
+    
+    /usr/bin/clang-format-${CLANG_FORMAT_VERSION} -n --Werror --style=file --fallback-style=Google ${files} || local format_status=$?
+		
+	if [[ "${format_status}" -ne 0 ]]; then
+		log_ "Run clang-format before commit"
+		return ${format_status}
+	fi
+	
+}
+
+cd ${GITHUB_WORKSPACE}
+
+install_clang
+
+clang_file || exit_code=$?
+
+exit $exit_code 
